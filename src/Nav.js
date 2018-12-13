abdac9cc07dea6c1ef0195e3c2c0bc5da38198e9
cecil john tantay <cjbtantay@gmail.com>
2018-12-12 23:29:28 -0800

    passed in props to a class form apps.js.. good practice

--- src/Nav.js
+++ src/Nav.js
@@ -4,27 +4,63 @@ import { NavLink } from "react-router-dom";
 import burger from "./img/nav-hamburger.png";
 import burgerClose from "./img/nav-hamburger-close.png";
 
-const Nav = (props)=> {
- 
+
+class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+    // reference to the DOM node
+    // this.myElement = null;
+    // reference to the animation
+    // this.myTween = null;
+  }
+  // componentDidMount(){
+  //   // use the node ref to create the animation
+  //   // this.myTween = TweenLite.to(this.myElement, 1, {x: 100, y: 100});
+  //   TweenLite.to(this.myElement, .3, {x: 5, y: 5});
+  // }
+
+
+    // return <div ref={div => this.myElement = div} />;
+
+  render() {
     return (
-      <div className="nav-bar">
-        <div className={props.onLoad ? "nav--clicked nav-container" : "nav-container"}>
-        <NavLink to="/" className="navh2"><h2>S&J</h2></NavLink>
-          <nav className={props.onLoad ? "nav-open" : "nav-close"}>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/services">Services</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
+      // //className="nav-bar" ref={div => this.myElement = div}
+      <div > 
+        <div
+          className={
+            this.props.onLoad ? "nav--clicked nav-container" : "nav-container"
+          }
+        >
+          <NavLink to="/" className="navh2">
+            <h2>S&J</h2>
+          </NavLink>
+          <nav className={this.props.onLoad ? "nav-open" : "nav-close"}>
+            <NavLink to="/" onClick={() => this.props.handleChange()}>
+              Home
+            </NavLink>
+            <NavLink to="/services" onClick={() => this.props.handleChange()}>
+              Services
+            </NavLink>
+            <NavLink to="/contact" onClick={() => this.props.handleChange()}>
+              Contact
+            </NavLink>
           </nav>
-          <div className="burger" onClick={ ()=>props.handleChange()}>
-            <img src={burger} alt="hamburger menu img" className={props.onLoad ? "nav-close" : "nav-open"} />
-            <img src={burgerClose} alt="" className={props.onLoad ? "nav-open" : "nav-close"}  />
+          <div className="burger" onClick={() => this.props.handleChange()}>
+            <img
+              src={burger}
+              alt="hamburger menu img"
+              className={this.props.onLoad ? "nav-close" : "nav-open"}
+            />
+            <img
+              src={burgerClose}
+              alt=""
+              className={this.props.onLoad ? "nav-open" : "nav-close"}
+            />
           </div>
         </div>
       </div>
     );
   }
-
-
+}
 
 export default Nav;
-
