7fe89eb3a6fedbb3d63795afc784cd6f7fb085a4
cecil john tantay <cjbtantay@gmail.com>
2018-12-12 23:33:40 -0800

    revised code

--- src/Nav.js
+++ src/Nav.js
@@ -4,27 +4,13 @@ import { NavLink } from "react-router-dom";
 import burger from "./img/nav-hamburger.png";
 import burgerClose from "./img/nav-hamburger-close.png";
 
-
 class Nav extends React.Component {
   constructor(props) {
     super(props);
-    // reference to the DOM node
-    // this.myElement = null;
-    // reference to the animation
-    // this.myTween = null;
   }
-  // componentDidMount(){
-  //   // use the node ref to create the animation
-  //   // this.myTween = TweenLite.to(this.myElement, 1, {x: 100, y: 100});
-  //   TweenLite.to(this.myElement, .3, {x: 5, y: 5});
-  // }
-
-
-    // return <div ref={div => this.myElement = div} />;
-    // className="nav-bar" ref={div => this.myElement = div}
   render() {
     return (
-      <div >
+      <div className="nav-bar">
         <div
           className={
             this.props.onLoad ? "nav--clicked nav-container" : "nav-container"
