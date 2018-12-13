6b201a9d1e7e565d4d376dfe12a446ae560a15ff
cecil john tantay <cjbtantay@gmail.com>
2018-12-13 08:56:39 -0800

    added animations to app

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./index.css";
-import Home from './Home';
-import Services from './Services';
+import Home from "./Home";
+import Services from "./Services";
 import Contact from "./Contact";
 import Error from "./Error";
 import Nav from "./Nav";
 import Footer from "./Footer";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       onLoad: false
-    }
+    };
   }
-  handleChange = ()=> {
-    this.setState(prevState => ({onLoad: !prevState.onLoad}))
-  }
+  handleChange = () => {
+    this.setState(prevState => ({ onLoad: !prevState.onLoad }));
+  };
 
   render() {
     return (
       <BrowserRouter>
-        <div>
-          <Nav onLoad={this.state.onLoad} handleChange={this.handleChange}/>
+        <div >
+        
+          <Nav onLoad={this.state.onLoad} handleChange={this.handleChange} />
           <Switch>
-            <Route path="/" component={Home} exact/>
-            <Route path="/services" component={Services}/>
-            <Route path="/contact" component={Contact}/>
-            <Route component={Error}/>
+            <Route path="/" component={Home} exact />
+            <Route path="/services" component={Services} />
+            <Route path="/contact" component={Contact} />
+            <Route component={Error} />
           </Switch>
           <Footer />
         </div>
